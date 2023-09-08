28f19bfc7a74ef3de4a795d8e3a085c8aa3edeba
zainebguetif <zguetif90@gmail.com>
2023-09-08 10:47:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - it prints out output using put
+*
+*Return: always 0 (success)
+*
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
