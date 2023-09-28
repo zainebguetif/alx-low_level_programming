9aee23b9afcd13033cca84ec6643057cd35ac797
zainebguetif <zguetif90@gmail.com>
2023-09-28 03:38:19 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar('\n');
+		_putchar(*s);
+		_put_recursion(s+1);
 	}
 	else
-	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-	}
+	_putchar('\n');
 }
