ca0df875b80da1c503eeac60cbc100b510cb95b3
zainebguetif <zguetif90@gmail.com>
2023-10-12 12:20:08 -0700

    0-sum_them_all.c

--- 0-sum_them_all.c/0-sum_them_all.c
+++ 0-sum_them_all.c/0-sum_them_all.c
@@ -9,14 +9,22 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
+
 	unsigned int i;
-	int sum = 0;
+	unsigned int sum = 0;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(valist,  int);
+		if (n == 0)
+		{
+			return (0);
+		}
+		else 
+		{
+		sum += va_arg(valist, const unsigned int);
+		}
 	}
 
 	va_end(valist);
